fde9851b2ab6ec82b5ad4064db3ff729a9e6b34e
Nelis Pereira <nelisnnp@gmail.com>
2022-07-06 12:20:03 -0300

    Projeto Default com o Prisma e alguns erros configurado

---
+++ src/common/errors/interceptors/unauthorized.interceptor.ts
@@ -0,0 +1,17 @@
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler, UnauthorizedException } from '@nestjs/common';
+import { catchError, Observable } from 'rxjs';
+
+@Injectable()
+export class UnauthorizedInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      catchError(error => {
+        if (error instanceof UnauthorizedException) {
+          throw new UnauthorizedException(error.message);
+        } else {
+          throw error;
+        }
+      }),
+    );
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConflictInterceptor } from './common/errors/interceptors/conflict.interceptor';
+import { DatabaseInterceptor } from './common/errors/interceptors/database.interceptor';
+import { NotFoundInterceptor } from './common/errors/interceptors/notfound.interceptor';
+import { UnauthorizedInterceptor } from './common/errors/interceptors/unauthorized.interceptor';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new ConflictInterceptor());
+  app.useGlobalInterceptors(new DatabaseInterceptor());
+  app.useGlobalInterceptors(new UnauthorizedInterceptor());
+  app.useGlobalInterceptors(new NotFoundInterceptor());
+  await app.listen(process.env.PORT);
+}
+bootstrap();
